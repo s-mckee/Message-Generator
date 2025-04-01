56bd6a9f406d09cd61a2b8ee87f13bff00dc0829
Sarah Mckee <sarahmckee714@gmail.com>
2025-04-01 16:24:53 -0500

    Added components object to store the arrays of message components

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // Create a computer-generated mad-libs
-// Array for verbs, nouns, and adjectives
-// Maybe some arrays for random kid names and ages????
 // Topic: Silly things kids say
 // Give it a fancy appearance
 // Function that creates and returns strings using random number generation to grab elements from the arrays
+const components = {
+  objects: ["bowl", "cookie", "bucket", "sandwich", "truck", "dolly", "dress"],
+  animals: ["duck", "cat", "dog", "cow", "hamster", "chicken"],
+  titles: ["knight", "police officer", "princess", "robber"],
+  verbs: ["fell into", "got stuck in", "threw away", "ate", "farted on"],
+  adjectives: ["pretty", "brave", "smart", "tall", "strong", "fat"],
+  names: ["Jill", "Ember", "Lily", "Kevin", "Todd", "Marshall"],
+};
