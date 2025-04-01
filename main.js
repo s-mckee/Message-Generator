122a70d3bd1529b4a8439a626772c3f9b3e330d9
Sarah Mckee <sarahmckee714@gmail.com>
2025-04-01 16:16:02 -0500

    Added planning comments

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+// Create a computer-generated mad-libs
+// Array for verbs, nouns, and adjectives
+// Maybe some arrays for random kid names and ages????
+// Topic: Silly things kids say
+// Give it a fancy appearance
+// Function that creates and returns strings using random number generation to grab elements from the arrays
